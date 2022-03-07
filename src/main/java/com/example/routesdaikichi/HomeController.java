8726d28748bd6156d6472282128e5e32649abacd
amberdevpe <amberdevpe@gmail.com>
2022-03-07 00:47:14 -0500

    update routes

--- src/main/java/com/example/routesdaikichi/HomeController.java
+++ src/main/java/com/example/routesdaikichi/HomeController.java
@@ -4,6 +4,7 @@ package com.example.routesdaikichi;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/daikichi")
@@ -20,4 +21,15 @@ public class HomeController {
     public String tomorrow() {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
+
+    @RequestMapping(value="/travel/{destination}", method = RequestMethod.GET)
+    public String travel(@PathVariable String destination) {
+        return "Congratulations! You will soon travel to " + destination;
+    }
+
+    @RequestMapping(value="/lotto/{number}", method = RequestMethod.GET)
+    public String lotto(@PathVariable int number) {
+        return (number %2 == 0) ? "You will take a grand journey in the near future, but be wory of tempting ofers" : "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends..";
+    }
 }
+
